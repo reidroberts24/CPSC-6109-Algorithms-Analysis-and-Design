928a7998871a739f04f57775f1ebe77cb816f25e
Reid Roberts <reidroberts24@gmail.com>
2025-01-19 22:57:37 -0600

    implement heapsort and insertion sort

--- Assignment_02_Big_Oh_Exploration/HeapSort.java
+++ Assignment_02_Big_Oh_Exploration/HeapSort.java
@@ -1,5 +1,49 @@
 package Assignment_02_Big_Oh_Exploration;
 
 public class HeapSort {
-    
+    private static int heapSize;
+    // max-heapify method to maintain max heap property
+    private static void maxHeapify(int[] arr, int i) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int largest = i;
+
+        if (left < heapSize && arr[left] > arr[largest]) {
+            largest = left;
+        } else {
+            largest = i;
+        }
+
+        if (right < heapSize && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            int tmp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = tmp;
+            maxHeapify(arr, largest);
+        }
+    }
+
+    //build max-heap method to create heap from array
+    private static void buildMaxHeap(int[] arr, int n) {
+        heapSize = n;
+        for (int i = (n / 2); i >= 0; i--) {
+            maxHeapify(arr, i);
+        }
+    }
+
+
+    //sort max-heap
+    public static void sort(int[] arr, int n) {
+        buildMaxHeap(arr, n);
+        for (int i = n - 1; i > 0; i--) {
+            int tmp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = tmp;
+            heapSize--;
+            maxHeapify(arr,0);
+        }
+    }
 }
--- Assignment_02_Big_Oh_Exploration/main.java
+++ Assignment_02_Big_Oh_Exploration/main.java
@@ -1,21 +1,39 @@
 package Assignment_02_Big_Oh_Exploration;
 import java.util.Arrays;
+import java.util.Random;
 public class main {
     public static void main(String[] args) {
 
         long startTime; 
         long endTime;
         long duration;
-        //create a sample of array integers
-        int[] array = {5, 3, 6, 1, 7, 9};
 
-        System.out.println("Before sorting: " + Arrays.toString(array));
+        //create a sample of array integers
+        int n = 10000000;
+        int[] largeArray = new int[n];
+        Random rand = new Random();
+        for (int i = 0; i < n; i++) {
+            largeArray[i] = rand.nextInt();
+        }
+        int[] insertionSortArray = largeArray;
+        int[] heapSortArray = largeArray;
+      
+/*
+        //System.out.println("Before sorting: " + Arrays.toString(insertionSortArray));
         startTime = System.currentTimeMillis();
-        InsertionSort.sort(array);
+        InsertionSort.sort(insertionSortArray);
         endTime = System.currentTimeMillis();
         duration = endTime - startTime;
-        System.out.println("After sorting: " + Arrays.toString(array));
-        System.out.println("Insertion sort took: " + duration);
+        //System.out.println("After sorting: " + Arrays.toString(insertionSortArray));
+        System.out.println("Insertion sort took: " + duration + "ms");
+*/
 
+        //System.out.println("Before HeapSort: " + Arrays.toString(heapSortArray));
+        startTime = System.currentTimeMillis();
+        HeapSort.sort(heapSortArray, heapSortArray.length);
+        endTime = System.currentTimeMillis();
+        duration = endTime - startTime;
+        //System.out.println("After sorting: " + Arrays.toString(heapSortArray));
+        System.out.println("Heapsort took: " + duration + "ms");
     }
 }
