c482fd05494adea65e83cba7ceaa7d8002d83179
Reid Roberts <reidroberts24@gmail.com>
2025-01-20 11:00:11 -0600

    implement partitioning method

--- Assignment_02_Big_Oh_Exploration/QuickSort.java
+++ Assignment_02_Big_Oh_Exploration/QuickSort.java
@@ -1,26 +1,30 @@
 package Assignment_02_Big_Oh_Exploration;
 
 public class QuickSort {
-    public static void sort(int[] arr, int p, int r) {
+    public static void sort(int[] arr, int p, int r) { //input array, initial index p, length of array r
         if (p < r) {
             int q = partition(arr, p, r);
+            sort(arr, p, q - 1);
+            sort(arr, q + 1, r);
         }
     }
-
-    private static int partition(int[] arr, int p, int r) { //partition helper method
-        int x = arr[r]; //set pivot point
+    
+    //partition the array in place to get high and low side about index i+1
+    private static int partition(int[] arr, int p, int r) { 
+        int x = arr[r]; //set pivot point as last element
         int i = p - 1; //highest index on the low side
-        for (int j = p; j < r; j++) { // iterate from the pivot to the end of the array, j is the index being evaluated
-            if (arr[j] <= x) { //if the current element at j is less than the pivot, swap them
+        for (int j = p; j < r; j++) { // iterate from the current element to the end of the array (to the pivot) 
+            if (arr[j] <= x) { //if element at j is less than the pivot, they need to be swapped
                 i++;
-                int tmp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = tmp;
+                int tmp = arr[i]; //store element at i as tmp variable
+                arr[i] = arr[j]; //swap i with j
+                arr[j] = tmp; //set j as tmp
             }
         }
-        int tmp2 = arr[i + 1];
-        arr[i + 1] = x;
-        x = tmp2;
-        return i + 1;
+        //swap the pivot element with the i+1 element so we have a low side and a high side
+        int tmp2 = arr[i + 1]; //store i+1 element as tmp2
+        arr[i + 1] = x; //swap i+1 element with pivot
+        x = tmp2; //set final array element as tmp2 
+        return i + 1; //return i+1 because that is where the final element on the low side is 
     }
 }
