68e30aa440ebacea2f754fa2ff15c6c6f23963b8
Reid Roberts <reidroberts24@gmail.com>
2025-01-20 10:28:45 -0600

    create quicksort

--- Assignment_02_Big_Oh_Exploration/QuickSort.java
+++ Assignment_02_Big_Oh_Exploration/QuickSort.java
@@ -1,5 +1,26 @@
 package Assignment_02_Big_Oh_Exploration;
 
 public class QuickSort {
-    
+    public static void sort(int[] arr, int p, int r) {
+        if (p < r) {
+            int q = partition(arr, p, r);
+        }
+    }
+
+    private static int partition(int[] arr, int p, int r) {
+        int x = arr[r];
+        int i = p - 1;
+        for (int j = p; j < r; j++) {
+            if (arr[j] <= x) {
+                i++;
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+        int tmp2 = arr[i + 1];
+        arr[i + 1] = x;
+        x = tmp2;
+        return i + 1;
+    }
 }
