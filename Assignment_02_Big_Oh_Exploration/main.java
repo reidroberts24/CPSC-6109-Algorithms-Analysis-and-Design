d0f9323ee9f4eba4740ba022b97c6f6662e26649
Reid Roberts <reidroberts24@gmail.com>
2025-01-19 20:27:24 -0600

    compiled

--- Assignment_02_Big_Oh_Exploration/main.java
+++ Assignment_02_Big_Oh_Exploration/main.java
@@ -1,5 +1,21 @@
 package Assignment_02_Big_Oh_Exploration;
-
+import java.util.Arrays;
 public class main {
-    
+    public static void main(String[] args) {
+
+        long startTime; 
+        long endTime;
+        long duration;
+        //create a sample of array integers
+        int[] array = {5, 3, 6, 1, 7, 9};
+
+        System.out.println("Before sorting: " + Arrays.toString(array));
+        startTime = System.currentTimeMillis();
+        InsertionSort.sort(array);
+        endTime = System.currentTimeMillis();
+        duration = endTime - startTime;
+        System.out.println("After sorting: " + Arrays.toString(array));
+        System.out.println("Insertion sort took: " + duration);
+
+    }
 }
