fd61a1cc8d672e60eabea9797c0f57da671364d1
Reid Roberts <reidroberts24@gmail.com>
2025-01-21 12:14:44 -0600

    comments

--- Assignment_02_Big_Oh_Exploration/main.java
+++ Assignment_02_Big_Oh_Exploration/main.java
@@ -58,6 +58,7 @@ public class main {
         
     }
 
+    //test case to verify that each sorting function properly sorted its array
     public static boolean isSorted(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] > arr[i+1]) {
